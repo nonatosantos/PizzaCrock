501e4af79097281b6d4d579fe1b3a8acd3d573fd
NONATO SANTOS <nonatosantos1986@gmail.com>
2019-05-19 23:02:11 -0300

    Refatorando OrderViewModel

--- PizzaCrock.Domain/ViewModels/OrderViewModel.cs
+++ PizzaCrock.Domain/ViewModels/OrderViewModel.cs
@@ -1,8 +1,5 @@
 ﻿using PizzaCrock.Domain.Entities;
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 
 namespace PizzaCrock.Domain.ViewModels
 {
@@ -15,18 +12,7 @@ namespace PizzaCrock.Domain.ViewModels
         public string Flavor { get; set; }
         public int PreparationTime { get; set; }
         public decimal TotalPrice { get; set; }
-        public IList<Additional> Additionals { get; set; }
-        public int AdditionalTime { get; set; }
-
-        private int TimeAdditional()
-        {
-            return Additionals.ToList().Sum(x => x.AdditionalMinutes);
-        }
-
-        private decimal PriceAdditional()
-        {
-            return Additionals.ToList().Sum(x => x.AdditionalPrice);
-        }
+        public IList<Additional> Additionals { get; set; }  
 
 
     }
